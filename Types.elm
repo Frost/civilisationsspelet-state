69e75866ec68e0781dafae14f6f520e3fb02dd0a
Martin Frost <martin.frost@kundo.se>
2017-04-16 14:50:05 +0200

    Add ResourceId and TechnologyId types
    These will be used to simplify the Resource and Technology types and
    dependencies between them.

--- Types.elm
+++ Types.elm
@@ -21,6 +21,62 @@ type TechnologyEffect = EnableResourceType ResourceType
                       | DefensiveBonus Int
                       | MilitaryBonus Int
 
+type ResourceId = Beans
+                | Beef
+                | Camel
+                | Cassava
+                | Chicken
+                | Chickpeas
+                | Corn
+                | Cotton
+                | Donkey
+                | Durra
+                | Elephant
+                | Hemp
+                | Horse
+                | Jute
+                | Linen
+                | Llama
+                | Millet -- Hirs
+                | Oats
+                | Pork
+                | Potatoe
+                | Rice
+                | Rye
+                | Sheep
+                | Silk
+                | SoyBean
+                | Sugar
+                | WaterBuffalo
+                | Wheat
+                | Yam
+
+type TechnologyId = Astronomy
+                  | BlackPowder
+                  | BookPress
+                  | BookReligion
+                  | Bureaucracy
+                  | Companies
+                  | Credit
+                  | Drama
+                  | Engineering
+                  | Irrigation
+                  | Law
+                  | Litterature
+                  | Machines
+                  | Mathematics
+                  | MetalTooling
+                  | Nationalism
+                  | Philosophy
+                  | PlantBreeding
+                  | Priesthood
+                  | Science
+                  | Weaving
+                  | Wheel
+                  | Writing
+
+type TechnologyEffectCondition = ResourceNeeded List ResourceId
+
 type alias ClimateZone = (ClimateZoneType, String)
 
 type alias Produce = ( ClimateZoneType, Float )
