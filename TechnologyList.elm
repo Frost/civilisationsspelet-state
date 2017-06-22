7158f512176c67596b0c6dacd56a91c35329a8a2
Martin Frost <martin.frost@kundo.se>
2017-06-22 22:29:35 +0200

    Disable unavailable technologies
    Disable the checkboxes next to the disabled technologies and use gray text for
    them.

--- TechnologyList.elm
+++ TechnologyList.elm
@@ -1,8 +1,8 @@
 module TechnologyList exposing (view, addTechnology, removeTechnology)
 
-import Html exposing (Html, ul, li, text, section, h1, label, input)
+import Html exposing (Html, ul, li, text, section, h1, label, input, span)
 import Html.Events exposing (onClick, onMouseOver, onMouseOut)
-import Html.Attributes exposing (type_, checked, for, style)
+import Html.Attributes exposing (type_, checked, for, style, disabled)
 import Types exposing (Player, Technology, Msg, TechnologyId)
 import Resources exposing (technologies, technologyDisplayData, technologyName)
 
@@ -32,14 +32,19 @@ items player =
 
 item : Player -> Technology -> Html Msg
 item player technology =
-    let check = hasTechnology technology player in
+    let
+        check = hasTechnology technology player
+        isDisabled = technologyIsUnavailable technology player
+    in
       li []
           [ label [ onMouseOver (displayTechnologyDetail technology.id), onMouseOut displayNoTechnologyDetail ]
-                [ input [ type_ "checkbox", checked check, onClick (msg check technology)] []
-                , text <| technologyName technology.id
-                , text " ("
-                , text <| toString <| cost technology <| List.map (\{id} -> id) player.technologies
-                , text ")"
+                [ input [ type_ "checkbox", checked check, onClick (msg check technology), disabled isDisabled] []
+                , span [ style [("color", displayColor isDisabled)] ]
+                    [ text <| technologyName technology.id
+                    , text " ("
+                    , text <| toString <| cost technology <| playerTechnologyIds player
+                    , text ")"
+                    ]
                 ]
           ]
 
@@ -72,14 +77,30 @@ cost technology knownTechnologies =
     technology.cost + 2 * (List.length missingTechnologies)
 
 -- A technology is available if it has no requirements or if at least one of its requirements is known
-technologyIsAvailable : Technology -> List TechnologyId -> Bool
-technologyIsAvailable technology knownTechnologies =
+technologyIsUnavailable : Technology -> Player -> Bool
+technologyIsUnavailable technology player =
     case List.length(technology.requirements) of
         0 ->
-            True
+            False
         _ ->
-            let knownRequirements = List.filter (\id -> List.member id knownTechnologies) technology.requirements in
-            List.length(knownRequirements) > 0
+            List.length(knownRequirements player technology) == 0
 
 technologyProvides : Technology -> TechnologyId -> Bool
 technologyProvides technology wantedTechnology = List.member wantedTechnology technology.provides
+
+playerTechnologyIds : Player -> List TechnologyId
+playerTechnologyIds player = List.map (\{id} -> id) player.technologies
+
+knownRequirements : Player -> Technology -> List TechnologyId
+knownRequirements player {requirements} =
+    List.filter (\id -> List.member id <| playerTechnologyIds player) requirements
+
+missingRequirements : Player -> Technology -> List TechnologyId
+missingRequirements player {requirements} =
+    List.filter (\id -> not <| List.member id <| playerTechnologyIds player) requirements
+
+displayColor : Bool -> String
+displayColor isDisabled =
+    case isDisabled of
+        True -> "gray"
+        False -> "black"
