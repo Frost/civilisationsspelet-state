b8d35f79b26c54bd68e43fd9099a486cd4c8c5c4
Martin Frost <martin.frost@kundo.se>
2017-06-22 21:59:10 +0200

    Display requirements and provides in TechnologyDetail

--- TechnologyDetail.elm
+++ TechnologyDetail.elm
@@ -1,6 +1,6 @@
 module TechnologyDetail exposing (view)
 
-import Html exposing (Html, section, h1, h2, p, text)
+import Html exposing (Html, section, h1, h2, p, text, div, ul, li)
 import Html.Attributes exposing (style)
 import Types exposing (..)
 import Resources exposing (..)
@@ -18,10 +18,26 @@ technologyDetailView : TechnologyId -> Html Msg
 technologyDetailView id =
     case technologyDetailById id of
         Just (_, name, description) ->
-            section []
-                [ h1 [] [ text "Info: ", text name ]
-                , p [ style [("width", "20em")]] [ text description ]
-                ]
+            case technologyById id of
+                Just technology ->
+                    section []
+                        [ h1 [] [ text "Info: ", text name ]
+                        , p [ style [("width", "20em")]] [ text description ]
+                        , div []
+                            [ h2 [] [ text "Krav" ]
+                            , technologyIdList technology.requirements
+                            ]
+                        , div []
+                            [ h2 [] [ text "Ger tillgång till" ]
+                            , technologyIdList technology.provides
+                            ]
+                        ]
+                Nothing ->
+                    text ""
         Nothing ->
             text ""
 
+technologyIdList : List TechnologyId -> Html Msg
+technologyIdList ids =
+    ul []
+        (List.map (\id -> li [] [ text <| technologyName id]) ids)
